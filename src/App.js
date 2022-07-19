872fa2d6ad37b71c97f8d27d92b463eddb6e283f
MatthewLZR <mlopez@re7systems.com>
2022-07-19 03:59:17 +0200

    reformatComplete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import LoginPage from "./pages/LoginPage";
-import PrivateRoute from "./utils/PrivateRoute";
-import Home from "./pages/Home";
-import Layout from "./pages/Layout";
-import Unauthorized from "./pages/Unauthorized";
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Editor from './components/Editor';
+import Admin from './components/Admin';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import Lounge from './components/Lounge';
+import LinkPage from './components/LinkPage';
+import RequireAuth from './components/RequireAuth';
+import { Routes, Route } from 'react-router-dom';
 
 const ROLES = {
   'User': 2001,
@@ -14,22 +18,38 @@ const ROLES = {
 }
 
 function App() {
+
   return (
-    <div className="App">
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Layout/>}>
-              <Route path="login" exact element={<LoginPage />} />
-              <Route path="unauthorized" element={<Unauthorized />} />
-              <Route  element={<PrivateRoute allowedRoles={[ROLES.User,ROLES.Admin]}/>}>
-                <Route exact path="/home" element={<Home />} />
-              </Route>
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.User,ROLES.Editor]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+          <Route path="lounge" element={<Lounge />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
